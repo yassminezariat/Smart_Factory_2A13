9fe5142dd8bf49e2496ec17ae4e544565541ac90
yassmine <yassmine.zariat@esprit.tn>
2020-12-11 00:03:56 +0100

    login commit

---
+++ login.cpp
@@ -0,0 +1,29 @@
+#include "login.h"
+#include "ui_login.h"
+#include<QMessageBox>
+#include"mainwindow.h"
+login::login(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::login)
+{
+    ui->setupUi(this);
+}
+
+login::~login()
+{
+    delete ui;
+}
+
+void login::on_pushButton_clicked()
+{
+    bool test=a.login(ui->usename->text(),ui->password->text());
+    if(test){
+        MainWindow *w=new MainWindow();
+        w->show();
+        hide();
+    }
+    else{
+        QMessageBox::critical(nullptr,QObject::tr("Connexion"),QObject::tr("username or pwd incorrect!"),QMessageBox::Cancel);
+    }
+}
+
